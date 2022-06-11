f0b412da741da4f2bdd20bc143da42db3ab4396b
LeoBruno400 <leobruno400@gmail.com>
2022-06-11 20:35:24 -0300

    no anda nada

--- src/OrdenamientoPorInserción.java
+++ src/OrdenamientoPorInserción.java
@@ -1,6 +1,8 @@
 public class OrdenamientoPorInserción {
     public static void main(String[] args) {
-        int[] arr = {2,4,6,8,10,12,14,16,18,20};
+        int[] arr = {89,45,68,90,29,34,17};
+        insercion(arr);
+        mostrarArreglo(arr);
     }
 
     public static void insercion(int [] arr){
@@ -14,7 +16,19 @@ public class OrdenamientoPorInserción {
                     arr[j] = arr[j-1];
                     j = j-1;
                 }
-                arr[j]=aux
+                arr[j]=aux;
         }
+    }
+
+    public static void mostrarArreglo(int[] arr) {
+        //Este modulo se encargar de mostrar por pantalla el arreglo
+        int i, longitud;
+        longitud = arr.length;
+        System.out.println("\n");
+        System.out.println("Los datos del arreglo son");
+        for (i = 0; i < longitud; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("\n");
     }
 }
