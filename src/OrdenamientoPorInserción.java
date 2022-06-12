685261691e0672d55838d9421bcc9516ae09cd37
LeoBruno400 <leobruno400@gmail.com>
2022-06-11 22:06:19 -0300

    a

--- src/OrdenamientoPorInserción.java
+++ src/OrdenamientoPorInserción.java
@@ -1,6 +1,6 @@
 public class OrdenamientoPorInserción {
     public static void main(String[] args) {
-        int[] arr = {89,45,68,90,29,34,17};
+        int[] arr = { 89, 45, 68, 90, 29, 34, 17 };
         System.out.println("El arreglo original es: ");
         mostrarArreglo(arr);
         insercion(arr);
@@ -8,24 +8,23 @@ public class OrdenamientoPorInserción {
         mostrarArreglo(arr);
     }
 
-    public static void insercion(int [] arr){
+    public static void insercion(int[] arr) {
         int i, j, aux;
         int longitud = arr.length;
 
-        for(i=1; i<=longitud-1; i++){
+        for (i = 1; i <= longitud - 1; i++) {
             aux = arr[i];
-            j=i;
-
-                while (j>0 && arr[j-1] > aux) {
-                    arr[j] = arr[j-1];
-                    j = j-1;
-                }
-                arr[j]=aux;
+            j = i;
+            while (j > 0 && arr[j - 1] > aux) {
+                arr[j] = arr[j - 1];
+                j = j - 1;
+            }
+            arr[j] = aux;
         }
     }
 
     public static void mostrarArreglo(int[] arr) {
-        //Este modulo se encargar de mostrar por pantalla el arreglo
+        // Este modulo se encargar de mostrar por pantalla el arreglo
         int i, longitud;
         longitud = arr.length;
         for (i = 0; i < longitud; i++) {
